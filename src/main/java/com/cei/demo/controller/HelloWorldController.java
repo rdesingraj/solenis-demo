3e9c325822a86344bf7d5e22d7faa7be7327ea8c
rdesingraj_CEIUS <rdesingraj@ceiamerica.com>
2023-04-25 16:15:27 +0530

    initial commit

--- src/main/java/com/cei/demo/controller/HelloWorldController.java
+++ src/main/java/com/cei/demo/controller/HelloWorldController.java
@@ -1,13 +1,13 @@
 package com.cei.demo.controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-@RestController
-public class HelloWorldController 
-{
-@RequestMapping("/")
-public String hello() 
-{
-return "Hotfix changes 1.0.1 new update.";
 
-}
+@RestController
+public class HelloWorldController {
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello.";
+
+    }
 }
