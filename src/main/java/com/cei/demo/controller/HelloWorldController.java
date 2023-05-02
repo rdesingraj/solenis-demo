8bf12a436dd25e8fdcc1851c7f709c846f79248c
rdesingraj <66407497+rdesingraj@users.noreply.github.com>
2023-05-02 18:12:26 +0530

    Feature 1 changes (#4)
    Co-authored-by: desing-solenis <DRajaram@contractor.solenis.com>

--- src/main/java/com/cei/demo/controller/HelloWorldController.java
+++ src/main/java/com/cei/demo/controller/HelloWorldController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @RequestMapping("/")
     public String hello() {
-        return "Hello.";
+        return "Jira 1 changes.";
 
     }
 }
